2d2424997eaf0f0853354f68df95fe28450078a7
Alexey Strokach <strokach@google.com>
2019-06-20 15:40:53 -0700

    Add `--interactive` option for running in interactive mode

--- sdks/python/apache_beam/options/pipeline_options.py
+++ sdks/python/apache_beam/options/pipeline_options.py
@@ -362,6 +362,10 @@ class StandardOptions(PipelineOptions):
                         default=False,
                         action='store_true',
                         help='Whether to enable streaming mode.')
+    parser.add_argument('--interactive',
+                        default=False,
+                        action='store_true',
+                        help='Whether to enable interactive mode.')
 
 
 class TypeOptions(PipelineOptions):
